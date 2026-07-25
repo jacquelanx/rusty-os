9584595ac94d3851b430a1381266c0a0eba96787
JacquelineXiong <jacqueline.lanx@gmail.com>
2026-07-25 13:47:26 -0700

    Bare metal freestanding binary setup complete; very minimal

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-to compile the crate: cargo build
+to compile the crate: `cargo build --target thumbv7em-none-eabihf` because we need to compile for a bare metal target without any underlying OS
 
 then run the compiled "rusty: binary in the target/debug subfolder.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,24 @@
-#![no_std]
+#![no_std]  // don't link Rust standard library
+#![no_main]  // disable all Rust-level entry points
+
 /* Storytime... core contains the pieces of Rust that don't depend on an OS;
 STD is built on core. Inside the core crate is a module called panic, which
 contains a struct called PanicInfo. The use keyword is for ease of importing.
 */
 use core::panic::PanicInfo;
+
+
+/* Normally, runtime procedures must happen before the main function is called
+(eg. C runtime libraries, which invoke the entry point of the Rust runtime marked
+by the _start item, which calls the main function). We don't have access to that
+so we need to define our own entry point. The linker looks for a function called
+`_start` by default.
+*/
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
 
 /* Since we removed the STD, we need to define the panic handler ourselves.
 The panic handler is the function that the compiler calls when a panic occurs.
@@ -19,8 +34,4 @@ NOTHING, it's NEVER return.
 // HOWEVER the & is NOT &mut, so you can't modify PanicInfo
 fn panic(_info: &PanicInfo) -> ! { 
     loop {}  // rip long comments #learning
-}
-
-fn main() {
-    // nothing for now
 }
