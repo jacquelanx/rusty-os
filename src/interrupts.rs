a96279236e4c2a6b2dd69f69a77b3c459d01340e
JacquelineXiong <jacqueline.lanx@gmail.com>
2026-08-16 15:25:52 -0700

    added handler function for page faults in interrupts.rs

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,6 +1,7 @@
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 use crate::println;
 use crate::print;
+use crate::hlt_loop;
 use crate::gdt;
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
@@ -39,6 +40,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         unsafe {
             idt.double_fault.set_handler_fn(double_fault_handler)
                 .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX);
@@ -55,6 +57,21 @@ lazy_static! {
 /* Handler function for breakpoint interrrupt. */
 extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+
+/* Handler function for page faults. */
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
 
 
